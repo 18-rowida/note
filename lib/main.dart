d9ab774b5bac8f4a64ce4731a31f51d670370af0
rowidaattia <76133788+18-rowida@users.noreply.github.com>
2024-01-30 16:20:22 +0200

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:note/views/notes_view.dart';
 
 void main() {
@@ -13,6 +14,7 @@ class NoteApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.dark,
+        
       ),
       home: const NotesView(),
     );
--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:note/views/widgets/custom_app_bar.dart';
+import 'package:note/views/widgets/custom_note_item.dart';
 
 class NoteViewBody extends StatelessWidget {
   const NoteViewBody({super.key});
@@ -14,6 +16,7 @@ class NoteViewBody extends StatelessWidget {
             height: 50,
           ),
           CustomAppBar(),
+          NoteItem(),
         ],
       ),
     );
