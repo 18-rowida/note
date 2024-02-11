c4cc870a90f6f53c65f6a61983780cceb8ef6468
Rowida Attia <>
2024-02-12 01:24:11 +0200

    create fetch note method

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:note/constants.dart';
-import 'package:note/cubits/cubit/add_note_cubit.dart';
+import 'package:note/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:note/models/note_model.dart';
 import 'package:note/simple_bloc_observer.dart';
 import 'package:note/views/notes_view.dart';
